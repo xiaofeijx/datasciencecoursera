301194109dba770126f9807d192a1c5cfe4d5592
Xiaofei Fu <7880774@qq.com>
2015-02-07 12:36:16 +0800

    data.table

--- dataclean.R
+++ dataclean.R
@@ -65,3 +65,39 @@ cat(myjson)
 #Convert back to JSON
 iris2 <- fromJSON(myjson)
 head(iris2)
+
+
+#lecture data.table
+library(data.table)
+DT = data.table(x=rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DT,3)
+
+tables()#查看data.table的情况
+
+DT[2,]
+DT[DT$y=="a",]
+DT[c(2,3)]#subset row
+
+DT[,list(mean(x),sum(z))]
+
+DT[,table(y)]
+
+DT[,w:=z^2] #adding new clumns
+DT[,m:={tmp <- (x+z);log2(tmp+5)}]
+DT[,a:= x>0]
+DT[,b:=mean(x+w),by=a]
+
+set.seed(123);
+DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
+DT[, .N, by=x]  #.N为计数
+
+#keys
+DT <- data.table(x=rep(c("a","b","c"),each=100), y=rnorm(300))
+setkey(DT, x)
+DT['a']
+
+#joins
+DT1 <- data.table(x=c('a', 'a', 'b', 'dt1'), y=1:4)
+DT2 <- data.table(x=c('a', 'b', 'dt2'), z=5:7)
+setkey(DT1, x); setkey(DT2, x)
+merge(DT1, DT2)
