9f4606ace5ef554de5adb84859f2a02bf6657ee0
xiaofeijx <7880774@qq.com>
2015-04-23 19:43:37 +0800

    ok

--- regression.R
+++ regression.R
@@ -545,3 +545,26 @@ summary(fit5)
 x <- -5:5
 y <- c(5.12, 3.93, 2.67, 1.87, 0.52, 0.08, 0.93, 2.05, 2.54, 3.87, 4.97)
 plot(x,y)
+knots <- 0
+splineTerms <- sapply(knots, function(knot) (x > knot) * (x - knot))
+xMat <- cbind(x, splineTerms)
+fit <- lm(y~xMat) 
+summary(fit)
+
+
+# simulate data
+n <- 500; x <- seq(0, 4 * pi, length = n); y <- sin(x) + rnorm(n, sd = .3)
+# define 20 knot points
+knots <- seq(0, 8 * pi, length = 20);
+range(x)
+# define the ()+ function to only take the values that are positive after the knot pt
+splineTerms <- sapply(knots, function(knot) (x > knot) * (x - knot))
+# define the predictors as X and spline term
+xMat <- cbind(x, splineTerms)
+head(xMat)
+# fit linear models for y vs predictors
+yhat <- predict(lm(y ~ xMat))
+# plot data points (x, y)
+plot(x, y, frame = FALSE, pch = 21, bg = "lightblue")
+# plot fitted values
+lines(x, yhat, col = "red", lwd = 2)
